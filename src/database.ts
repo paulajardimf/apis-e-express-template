be96206ee0dc1eccb9db4935142fa4b6536bfb02
Paula Jardim <paulajardimf@gmail.com>
2023-01-09 20:19:44 -0300

    exercicios finalizados

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,35 @@
-import { COURSE_STACK, TCourse } from "./types"
+import { COURSE_STACK, TCourse, TStudent } from "./types";
 
 export const courses: TCourse[] = [
-    {
-        id: "c001",
-        name: "React",
-        lessons: 12,
-        stack: COURSE_STACK.FRONT
-    },
-    {
-        id: "c002",
-        name: "Styled Components",
-        lessons: 4,
-        stack: COURSE_STACK.FRONT
-    },
-    {
-        id: "c003",
-        name: "Express",
-        lessons: 5,
-        stack: COURSE_STACK.BACK
-    }
-]
+  {
+    id: "c001",
+    name: "React",
+    lessons: 12,
+    stack: COURSE_STACK.FRONT,
+  },
+  {
+    id: "c002",
+    name: "Styled Components",
+    lessons: 4,
+    stack: COURSE_STACK.FRONT,
+  },
+  {
+    id: "c003",
+    name: "Express",
+    lessons: 5,
+    stack: COURSE_STACK.BACK,
+  },
+];
+
+export const students: TStudent[] = [
+  {
+    id: "e001",
+    name: "Ademara",
+    age: 25,
+  },
+  {
+    id: "e002",
+    name: "Camila",
+    age: 23,
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,78 @@
-import express, { Request, Response } from 'express'
-import cors from 'cors'
+import express, { Request, Response } from "express";
+import cors from "cors";
+import { courses, students } from "./database";
+import { COURSE_STACK, TCourse, TStudent } from "./types";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
-})
+  console.log("Servidor rodando na porta 3003");
+});
 
-app.get('/ping', (req: Request, res: Response) => {
-    res.send('Pong!')
-})
+app.get("/ping", (req: Request, res: Response) => {
+  res.send("Pong! pong pong");
+});
+
+app.get("/courses", (req: Request, res: Response) => {
+  res.status(200).send(courses);
+});
+
+app.get("/courses/search", (req: Request, res: Response) => {
+  const q = req.query.q as string;
+
+  const result = courses.filter((course) => {
+    return course.name.toLowerCase().includes(q.toLowerCase());
+  });
+
+  res.status(200).send(result);
+});
+
+app.post("/courses", (req: Request, res: Response) => {
+  // const id = req.body.id as string
+  // const name = req.body.name as string
+  // const lessons = req.body.lessons as number
+  // const stack = req.body.stack as COURSE_STACK
+
+  const { id, name, lessons, stack } = req.body as TCourse;
+
+  const newCourse = {
+    id,
+    name,
+    lessons,
+    stack,
+  };
+
+  courses.push(newCourse);
+
+  res.status(201).send("Curso registrado com sucesso!");
+});
+
+app.get("/students", (req: Request, res: Response) => {
+  res.status(200).send(students);
+});
+
+app.get("/students/search", (req: Request, res: Response) => {
+  const q = req.query.q as string;
+
+  const searchByName = students.filter((student) => {
+    return student.name.toLowerCase().includes(q.toLowerCase());
+  });
+
+  res.status(200).send(searchByName);
+});
+
+app.post("/students", (req: Request, res: Response) => {
+  const { id, name, age } = req.body as TStudent;
+
+  const newStudent: TStudent = {
+    id,
+    name,
+    age,
+  };
+
+  students.push(newStudent);
+  res.status(201).send("Estudante cadastrado com sucesso!");
+});
